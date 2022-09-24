4e94267a9f97865ebeb19bcbe4945322dd74a1b3
Jan Tuomi <jan.tuomi@valuemotive.com>
2022-09-24 19:23:40 +0300

    Add nice example code in test.lisp

--- examples/test.lisp
+++ examples/test.lisp
@@ -1,30 +1,9 @@
-;; (let map (\[f lst]
-;;     (match lst
-;;         [] []
-;;         (prepend (f (head lst)) (map f (tail lst))))))
+(let map (\[f lst]
+    (match lst
+        [] []
+        (prepend (f (head lst)) (map f (tail lst))))))
 
-;; (map (+ 1) [1 2 3])
-
-;; (let mapinc (\[vec]
-;;     (match vec
-;;         [] []
-;;         (prepend
-;;             (+ 1 (head vec))
-;;             (mapinc (tail vec))))))
-;; (env)
-;; (mapinc [1 2 3])
-
-;; (let fibo (\[n]
-;;     ;; (let fibo-1 (\[] (fibo (sub2 n 1))))
-;;     ;; (let fibo-2 (\[] (fibo (sub2 n 2))))
-;;     (match n
-;;         0  0
-;;         1  1
-;;         (+
-;;             (fibo (- n 1))
-;;             (fibo (- n 2))))))
-
-;; (fibo 10)
+(map (+ 1) [1 2 3])
 
 (let foldr (\[f accumulator lst]
     (match lst
@@ -32,3 +11,28 @@
         (f (foldr f accumulator (tail lst)) (head lst)))))
 
 (foldr + 0 [1 2 3])
+
+(let filter (\[pred lst]
+    (match lst
+        [] []
+        (match (pred (head lst))
+            true (prepend (head lst) (filter pred (tail lst)))
+            false (filter pred (tail lst))))))
+
+(let pred (\[n]
+    (match n
+        2 true
+        4 true
+        false)))
+
+(filter pred [0 1 2 3 4 5])
+
+(let fibo (\[n]
+    (match n
+        0  0
+        1  1
+        (+
+            (fibo (- n 1))
+            (fibo (- n 2))))))
+
+(fibo 10)
