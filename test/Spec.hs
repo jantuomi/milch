15b613ac76fa2a9a439f10ac9455c6eb1498b03f
Jan Tuomi <jans.tuomi@gmail.com>
2022-09-25 23:48:07 +0300

    Improve tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,13 +2,19 @@
 import Test.HUnit
 import Control.Monad.Except
 import Tokenizer ( tokenize )
+import Parser ( parse )
 import TestUtils
+import Utils
 
-test1 = (runLContext $ tokenize "+ 1 2")
-    >>= assertEqual "tokenize \"+ 1 2\"" ["+", "1", "2"]
+test1 = (runLContext $ tokenize "(+ 1 (- 10 5))")
+    >>= assertEqual "tokenize" ["(", "+", "1", "(", "-", "10", "5", ")", ")"]
+
+test2 = (runLContext $ parse ["(", "+", "1", "2", ")"])
+    >>= assertEqual "parse" [ASTFunctionCall [ASTSymbol "+", ASTInteger 1, ASTInteger 2]]
 
 tests = TestList $ map TestCase [
-    test1
+    test1,
+    test2
     ]
 
 main :: IO ()
