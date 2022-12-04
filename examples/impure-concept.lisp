9f4ca350ee81a874b0c838c2e82076efde6db8b2
Jan Tuomi <jans.tuomi@gmail.com>
2022-12-04 22:42:21 +0200

    Add impure concept

---
+++ examples/impure-concept.lisp
@@ -0,0 +1,12 @@
+; What color is your function?
+; Impure functions are functions that can call both pure and impure functions.
+; Pure functions can only call pure functions.
+; Impure function names end in an exclamation mark (!) by convention.
+; The top-level context is impure but has special rules
+;   related to (import!) and (let!).
+
+; pure function
+(let! f (\[x] x))
+
+; impure function
+(let! g (\![x] (print! x)))
